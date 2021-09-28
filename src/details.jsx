20d361ca6441866f18f0d92601830b113c21acfe
bashasohail7 <87037344+bashasohail7@users.noreply.github.com>
2021-09-28 10:54:16 +0530

    Update details.jsx

--- src/details.jsx
+++ src/details.jsx
@@ -4,26 +4,19 @@ import axios from 'axios'
 export default function Details() {
   var len=0;
     const[details,updatedetails]=useState([])
-
     const getdata=()=>{
 axios.get(' http://localhost:3001/inputdata')
 .then(res=>{
     axios.get(' http://localhost:3001/inputdata',{params:{id:res.data.length}})
     .then(result=>updatedetails(result.data))
 })
-
     }
-
     useEffect(()=>{getdata()},[true])
-
     return (
-        
         <div className='container mt-4'>
             <h3 className='text-dark text-center'>User Details</h3>
             {
-
              details.map((info,index)=>{
-                 
                 return(
                     <div key={index}>
                         <p><b>Fullname :</b> {info.fname} &nbsp; {info.lname}</p>
